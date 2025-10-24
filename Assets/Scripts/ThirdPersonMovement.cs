48939a5f4dc42ce70723321caf148b150ee30f0e
Zipster23 <86753446+Zipster23@users.noreply.github.com>
2025-10-24 10:17:12 -0400

    Commenting

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+    // Character Controller Game Component
     public CharacterController controller;
-    private PlayerInputController _playerInputController;
 
+    // Player Input Controller Script
+    private PlayerInputController playerInputController;
+
+    // Speed for movement
     [SerializeField]
     private float speed = 6f;
 
+    // Rate at which character turns to face in direction of movement
     [SerializeField]
     private float turnSmoothTime = 0.1f;
 
@@ -17,19 +22,27 @@ public class ThirdPersonMovement : MonoBehaviour
 
     private void Awake()
     {
-        _playerInputController = GetComponent<PlayerInputController>();
+        // Getting reference to PlayerInputController Script
+        playerInputController = GetComponent<PlayerInputController>();
     }
     private void Update()
     {
-        Vector3 direction = new Vector3(_playerInputController.MovementInputVector.x, 0, _playerInputController.MovementInputVector.y).normalized;
+        // Getting direction based on movement input
+        Vector3 direction = new Vector3(playerInputController.MovementInputVector.x, 0, playerInputController.MovementInputVector.y).normalized;
         
+        // If enough movement input, then move and turn
         if (direction.magnitude >= 0.1f)
         {
+            // Get angle to face
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+            // Smooth turn angle
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelcoity, turnSmoothTime);
 
+            // Rotate player
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            // Move the player
             controller.Move(direction * speed * Time.deltaTime);
         }
     }
