f8bcb9745d11ea62a314b2722c85da2e68d9acd6
joanasdasdasdasd <joanl8359@gmail.com>
2025-11-07 14:19:36 -0500

    Animation Update
    Added some comments to the CharacterAnimatorManager to make it more legible.
    
    Working on stamina next week :)

--- Assets/Scripts/Character/CharacterAnimatorManager.cs
+++ Assets/Scripts/Character/CharacterAnimatorManager.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class CharacterAnimatorManager : MonoBehaviour
 {
 
-    CharacterManager character;
+    CharacterManager character; // reference variable
 
     protected virtual void Awake()
     {
-        character = GetComponent<CharacterManager>();
+        character = GetComponent<CharacterManager>(); // sets the variable to the CharacterManager component.
     }
 
+    // Updates the animator's movement parameters to control which animations play based on player input
     public void UpdateAnimatorMovementParameters(float horizontalMovement, float verticalMovement)
     {
-        character.animator.SetFloat("Horizontal", horizontalMovement, 0.1f, Time.deltaTime);
-        character.animator.SetFloat("Vertical", verticalMovement, 0.1f, Time.deltaTime);
+        character.animator.SetFloat("Horizontal", horizontalMovement, 0.1f, Time.deltaTime);    // Sets the "Horizontal" animator parameter to the horizontal input value, smoothing the transition over 0.1 seconds
+        character.animator.SetFloat("Vertical", verticalMovement, 0.1f, Time.deltaTime);        // Sets the "Vertical" animator parameter to the vertical input value, smoothing the transition over 0.1 seconds        
     }
 
 }
